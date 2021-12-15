338911d2a747a7b6bbb9150f53d122370ec8ab7e
Ferdinand <holdenriederferdinand@gmail.com>
2021-12-15 17:01:11 +0100

    link auf details funzt

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,12 +5,12 @@ const Details = ({autos}) => {
     const { id } = useParams();
     console.log(id);
     const selectCar = autos.find(auto => auto.id === id)
-    console.log(selectCar)
+    console.log(autos);
     return (
         <div className='post, container'>
-            {/* <h2 className='title'>{selectCar.fields.name}</h2>
+             <h2 className='title'>{selectCar.name}</h2>
             {selectCar.id.featureimage && <img className='featureImage' src={selectCar.fields.featureImage.fields.file.url} alt={selectCar.fields.name} title={selectCar.fields.name} />}
-            <section dangerouslySetInnerHTML={{ __html: selectCar.id.discription }} /> */}
+            <section dangerouslySetInnerHTML={{ __html: selectCar.id.discription }} />
         </div>
     )
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import {Link} from "react-router-dom"; 
 
+
 const Products = ({autos}) => {
     console.log(autos);
+      
     return (
         <div>
             {autos.map((auto, index) => (        
                 <div className='post, container' key={index}>
                 <h2 className='title'>{auto.name}</h2>      
                 {auto.featureimage && <img className='featureImage' src={auto.featureimage} alt={auto.name} title={auto.name} />}
-                <Link to={auto.id}>Details</Link>
+                
+                <Link to={`/auto/${auto.id}`}>Details</Link>
                 {console.log(auto)}
                 </div>
+                
             ))}
+            
         </div>
     )
 }
